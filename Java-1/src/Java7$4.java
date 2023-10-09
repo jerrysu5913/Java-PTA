204096837bbbe3084b69f378263c413438e09ae0
JerrySu5379 <jerrysu5379@gmail.com>
2023-10-09 13:59:32 +0800

    update

---
+++ Java-1/src/Java7$4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Java7$4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for (int i = 1; i < num; i++){
+            for (int j = 2; j < num; j++){
+                if (i % j == 0 && num % j == 0){
+                    break;
+                }
+                else if (j == num - 1){
+                    System.out.print(i + "/" + num + ",");
+                }
+            }
+        }
+    }
+}
